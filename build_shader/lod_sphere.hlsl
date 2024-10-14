16ab0ea8a494add6c384bebd5e0f167ca8dec4e3
GyuBeom Han <sy0871@naver.com>
2024-10-14 22:50:27 +0900

    LOD Sphere

--- build_shader/lod_sphere.hlsl
+++ build_shader/lod_sphere.hlsl
@@ -99,83 +99,162 @@ VertexOut MidPoint(VertexOut v0, VertexOut v1)
     return mid;
 }
 
-void Subdivide(VertexOut inVertexs[3], out VertexOut outVertexs[6])
+void Subdivide(inout VertexOut outVertices[24], inout uint numVertices,
+    inout uint outIndexDatas[48], inout uint numIndices)
 {
-    outVertexs[0] = inVertexs[0];
-    outVertexs[1] = MidPoint(inVertexs[0], inVertexs[1]);
-    outVertexs[2] = MidPoint(inVertexs[2], inVertexs[0]);
-    outVertexs[3] = MidPoint(inVertexs[1], inVertexs[2]);
-    outVertexs[4] = inVertexs[2];
-    outVertexs[5] = inVertexs[1];
+    VertexOut inVertices[6];
+    uint inIndexDatas[12];
+    
+    // 기존 값 저장
+    [unroll]
+    for (uint vertex = 0; vertex < numVertices; ++vertex)
+    {
+        inVertices[vertex] = outVertices[vertex];
+    }
+    [unroll]
+    for (uint index = 0; index < numIndices; ++index)
+    {
+        inIndexDatas[index] = outIndexDatas[index];
+    }
+    
+    uint numTriangles = numIndices / 3;
+    
+    for (uint i = 0; i < numTriangles; ++i)
+    {
+        VertexOut v[3];
+        v[0] = inVertices[inIndexDatas[i * 3 + 0]];
+        v[1] = inVertices[inIndexDatas[i * 3 + 1]];
+        v[2] = inVertices[inIndexDatas[i * 3 + 2]];
+        
+        outVertices[i * 6 + 0] = v[0];
+        outVertices[i * 6 + 1] = v[1];
+        outVertices[i * 6 + 2] = v[2];
+        outVertices[i * 6 + 3] = MidPoint(v[0], v[1]);
+        outVertices[i * 6 + 4] = MidPoint(v[1], v[2]);
+        outVertices[i * 6 + 5] = MidPoint(v[0], v[2]);
+            
+        outIndexDatas[i * 12 + 0] = i * 6 + 0;
+        outIndexDatas[i * 12 + 1] = i * 6 + 3;
+        outIndexDatas[i * 12 + 2] = i * 6 + 5;
+        
+        outIndexDatas[i * 12 + 3] = i * 6 + 3;
+        outIndexDatas[i * 12 + 4] = i * 6 + 4;
+        outIndexDatas[i * 12 + 5] = i * 6 + 5;
+        
+        outIndexDatas[i * 12 + 6] = i * 6 + 5;
+        outIndexDatas[i * 12 + 7] = i * 6 + 4;
+        outIndexDatas[i * 12 + 8] = i * 6 + 2;
+        
+        outIndexDatas[i * 12 + 9] = i * 6 + 3;
+        outIndexDatas[i * 12 + 10] = i * 6 + 1;
+        outIndexDatas[i * 12 + 11] = i * 6 + 4;
+    }
+          
+    numVertices = 6 * numTriangles;
+    numIndices = 12 * numTriangles;
 }
 
-void ProjectionToSphere(float radius, inout VertexOut outVertexs[6])
+void ProjectionToSphere(float radius, uint numVertices, inout VertexOut outVertices[24])
 {
     // 정점들을 구에 투명 후 비례
-    for (int i = 0; i < 6; ++i)
+    for (uint i = 0; i < numVertices; ++i)
     {
         // 단위 구에 투영
-        float3 n = normalize(outVertexs[i].posL);
+        float3 n = normalize(outVertices[i].posL);
 
         // 주어진 반지름으로 비례(원래의 구에 투영)
         float3 p = radius * n;
 
-        outVertexs[i].posL = p;
-        outVertexs[i].normalL = n;
+        outVertices[i].posL = p;
+        outVertices[i].normalL = n;
         
         // 구면 좌표로부터 텍스처 좌표 계산
-        float theta = atan2(outVertexs[i].posL.z, outVertexs[i].posL.x);
+        float theta = atan2(outVertices[i].posL.z, outVertices[i].posL.x);
 
         // 각도 세타를 [0, 2pi]로 한정
         if (theta < 0.0f)
         {
             theta += 2.0f * PI_VALUE;
         }
-        float phi = acos(outVertexs[i].posL.y / PI_VALUE);
+        float phi = acos(outVertices[i].posL.y / PI_VALUE);
 
-        outVertexs[i].texCoord.x = theta / PI_VALUE;
-        outVertexs[i].texCoord.y = phi / PI_VALUE;
+        outVertices[i].texCoord.x = theta / PI_VALUE;
+        outVertices[i].texCoord.y = phi / PI_VALUE;
     }
 }
 
-void OutputSubdivision(VertexOut outVertexs[6], inout TriangleStream<GeoOut> triangleStream)
+void OutputSubdivision(VertexOut outVertices[24], uint numVertices,
+    uint outIndexDatas[48], uint numIndices, inout TriangleStream<GeoOut> triangleStream)
 {
-    GeoOut gout[6];
+    GeoOut gout[24];
     
     [unroll] // 실제로 반복하는 대신 별도로 처리해 성능 최적화
-    for (int i = 0; i < 6; ++i)
+    for (uint i = 0; i < numVertices; ++i)
     {
-        gout[i].posW = mul(float4(outVertexs[i].posL, 1.0f), gWorld).xyz;
-        gout[i].normalW = mul(outVertexs[i].normalL, (float3x3) gWorld);
+        gout[i].posW = mul(float4(outVertices[i].posL, 1.0f), gWorld).xyz;
+        gout[i].normalW = mul(outVertices[i].normalL, (float3x3) gWorld);
         
-        float4 texCoord = mul(float4(outVertexs[i].texCoord, 0.0f, 1.0f), gTexTransform);
+        float4 texCoord = mul(float4(outVertices[i].texCoord, 0.0f, 1.0f), gTexTransform);
         gout[i].texCoord = mul(texCoord, gMatTransform).xy;
         
         gout[i].posH = mul(float4(gout[i].posW, 1.0f), gViewProjection);
     }
     
-    [unroll]
-    for (int j = 0; j < 5; ++j)
+    for (uint t = 0; t < numIndices / 3; ++t)
     {
-        triangleStream.Append(gout[j]);
+        [unroll]
+        for (int j = 0; j < 3; ++j)
+        {
+            triangleStream.Append(gout[outIndexDatas[3 * t + j]]);
+        }
+        triangleStream.RestartStrip();
     }
-    triangleStream.RestartStrip();
-    
-    triangleStream.Append(gout[1]);
-    triangleStream.Append(gout[5]);
-    triangleStream.Append(gout[3]);
 }
 
 // 지오메트리 쉐이더
-[maxvertexcount(8)]
+[maxvertexcount(48)] 
 void GS(triangle VertexOut gin[3],
     uint primitiveID : SV_PrimitiveID,
     inout TriangleStream<GeoOut> triangleStream)
 {
-    VertexOut outVertexs[6];
-    Subdivide(gin, outVertexs);
-    ProjectionToSphere(length(gin[0].posL), outVertexs);
-    OutputSubdivision(outVertexs, triangleStream);
+    // 카메라의 eye와 중심 사이의 거리로 LOD 수준 설정 
+    float3 worldLocation = mul(float4(0.0f, 0.0f, 0.0f, 1.0f), gWorld).xyz;
+    float distance = length(gWorld._11_11_11 - gEyePosW);
+    
+    int lodLevel = 0;
+    if (distance < 50.0f)
+    {
+        lodLevel = 2;
+    }
+    else if (distance < 100.0f)
+    {
+        lodLevel = 1;
+    }
+    
+    VertexOut outVertices[24];
+    uint numVertices = 3;
+ 
+    uint outIndices[48];
+    uint numIndices = 3;
+    
+    // 기존 값 저장
+    [unroll]
+    for (uint vertex = 0; vertex < numVertices; ++vertex)
+    {
+        outVertices[vertex] = gin[vertex];
+    }
+    [unroll]
+    for (uint index = 0; index < numIndices; ++index)
+    {
+        outIndices[index] = index;
+    }
+    
+    for (int i = 0; i < lodLevel; ++i)
+    {
+        Subdivide(outVertices, numVertices, outIndices, numIndices);
+    }
+    ProjectionToSphere(length(gin[0].posL), numVertices, outVertices);
+    OutputSubdivision(outVertices, numVertices, outIndices, numIndices, triangleStream);
 }
 
 // 픽셀 쉐이더
